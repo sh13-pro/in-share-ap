53f716b30d5219d47da05926cbb2ca18d3e118ed
unknown <shristi.rani@btech.christuniversity.in>
2022-07-12 19:12:25 +0530

    add cors

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
+const cors = require('cors');
 app.use(express.static('public'));
 app.use(express.json());
 const connectDB = require('./config/db.js');
 connectDB();
-
+//cors
+const corsOptions ={
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+}
+app.use(cors(corsOptions));
 // template engine
 app.set('views',path.join(__dirname,'/views'));
 app.set('view engine', 'ejs');
